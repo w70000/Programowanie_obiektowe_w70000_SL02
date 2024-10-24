6f0e860d161ffd904bc32324b2fd4eb338a7e298
w70000 <w70000@RA117B-19>
2024-10-24 10:39:06 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var i = 10;
+            //i = "string";
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
