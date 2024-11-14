4d79b21f0353fdb2e94fbc3b192008847489d1e9
Emil Skarbowski <w70000@student.wsiz.edu.pl>
2024-11-14 10:40:22 +0100

    nowe

---
+++ Laboratorium_2/Laboratorium_2/Laboratorium/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Laboratorium_2.Laboratorium;
+
+Console.WriteLine("Hello, World!");
+
+
+Person os = new Person();
+
+os.Welcome("DD");
+os.Welcome("DD");
+
+
+
+
+
+
+
+
+
+
---
+++ Laboratorium_2/Laboratorium_2/Laboratorium/Zadanie_1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_2.Laboratorium
+{
+    public class Person
+    {
+
+        private string name;
+        private string surname;
+        private int age;
+        private string pesel;
+
+
+        public string Name { get { return name; } set { name = value + "TEST"; } }
+        public string Surname { get { return name; } set { name = value; } }
+
+        public int Age { get { return age; } 
+            set{
+               if (value > 0) age = value;
+               else age = 0;
+         }  }
+
+        public Person()
+        {
+            name = "Test";
+        }
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void Welcome(string otherName)
+        {
+            Console.WriteLine($"Witaj {otherName} Jestem {Name}");
+        }
+
+        public void Welcome(int otherName)
+        {
+            Console.WriteLine($"Witaj {otherName} Jestem {Name}");
+        }
+
+    }
+}
